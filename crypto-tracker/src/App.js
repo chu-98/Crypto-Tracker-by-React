92cf8a4c653d1494e94b406e08a04acb1b1b9d62
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-05-01 22:35:07 +0900

    STYLED COMPOENTS | ~ #2.6

--- crypto-tracker/src/App.js
+++ crypto-tracker/src/App.js
@@ -1,31 +1,27 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const animation = keyframes`
+0% {
+  transform:rotate(0deg);
+  border-radius: 0px;
+}
+50% {
+  transform:rotate(360deg);
+  border-radius: 100px;
+}
+100% {
+  transform:rotate(0deg);
+  border-radius: 0px;
+}`;
+
+const Wrapper = styled.div`
   display: flex;
-`;
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.h2`
-  color: white;
+  justify-content: center;
+  align-items: center;
 `;
 
 function App() {
-  return (
-    <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
-    </Father>
-  );
+  return <Wrapper></Wrapper>;
 }
 
 export default App;
