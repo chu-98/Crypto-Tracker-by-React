150ebcd46bc06d2c79093d061a908085c1368d18
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-05-02 12:15:06 +0900

    CRYPTO TRACKER | #4.0 Setup

--- crypto-tracker/src/index.js
+++ crypto-tracker/src/index.js
@@ -2,20 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const darkMode = {
-  textColor: "whitesmoke",
-  bgColor: "#111",
-};
-const lightMode = {
-  textColor: "#111",
-  bgColor: "whitesmoke",
-};
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightMode}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
