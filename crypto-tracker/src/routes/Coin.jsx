220611b1910e8e8ff940b6d6ecb72b37a663e9eb
chu-98 <81007078+chu-98@users.noreply.github.com>
2022-05-02 16:00:54 +0900

    CRYPTO TRACKER | #4.4 Route States

--- crypto-tracker/src/routes/Coin.jsx
+++ crypto-tracker/src/routes/Coin.jsx
@@ -1,9 +1,38 @@
-import { useParams } from "react-router";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
+
+const Container = styled.div``;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Title = styled.h1`
+  font-size: 48px;
+  margin: 36px 0px;
+  color: ${props => props.theme.accentColor};
+`;
+const Loader = styled.div`
+  text-align: center;
+  margin-top: 30px;
+`;
 
 function Coin() {
   const { coinID } = useParams();
+  const [loading, setLoading] = useState(true);
+  const { state } = useLocation();
 
-  return <h1>Coin: {coinID}</h1>;
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+        <Title>{state?.rank || "Loading..."}순위</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}{" "}
+    </Container>
+  );
 }
 
 export default Coin;
--- crypto-tracker/src/routes/Coins.jsx
+++ crypto-tracker/src/routes/Coins.jsx
@@ -18,9 +18,10 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 20px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in-out;
-    display: block;
   }
   :hover {
     color: ${props => props.theme.accentColor};
@@ -29,11 +30,17 @@ const Coin = styled.li`
 
 const Title = styled.h1`
   font-size: 48px;
+  margin: 36px 0px;
   color: ${props => props.theme.accentColor};
 `;
 const Loader = styled.div`
   text-align: center;
   margin-top: 30px;
+`;
+const Img = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 12px;
 `;
 
 function Coins() {
@@ -61,7 +68,15 @@ function Coins() {
         <CoinsList>
           {coins.map(coin => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.symbol} &rarr;</Link>
+              <Link
+                to={`/${coin.id}`}
+                state={{ name: coin.name, rank: coin.rank }}
+              >
+                <Img
+                  src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
